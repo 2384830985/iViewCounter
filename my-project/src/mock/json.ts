6d13ff0a8ff95855f34ab59c5438fd395667a323
tang <jinxintang@iyuedian.com>
2019-01-30 13:19:10 +0800

    添加 tags-view 的操作

---
+++ my-project/src/mock/json.ts
@@ -0,0 +1,56 @@
+const json = {
+    "icon": 'md-settings',
+    "name": '权限管理',
+    "url": '',
+    "child": [{
+        "icon": '',
+        "name": '权限管理',
+        "url": '/content/setting',
+        "child": []
+    },
+        {
+            "icon": '',
+            "name": '员工管理',
+            "url": '/content/setting',
+            "child": []
+        },
+        {
+            "icon": '',
+            "name": '角色管理',
+            "url": '/content/setting',
+            "child": [{
+                "icon": '',
+                "name": '1',
+                "url": '/content/setting',
+                "child": [{
+                    "icon": '',
+                    "name": '1',
+                    "url": '/content/setting',
+
+                },
+                    {
+                        "icon": '',
+                        "name": '2',
+                        "url": '/content/setting',
+                    },
+                    {
+                        "icon": '',
+                        "name": '3',
+                        "url": '/content/setting',
+                    },
+                ]
+            },
+                {
+                    "icon": '',
+                    "name": '2',
+                    "url": '/content/setting',
+                },
+                {
+                    "icon": '',
+                    "name": '3',
+                    "url": '/content/setting',
+                },
+            ]
+        },
+    ]
+};
